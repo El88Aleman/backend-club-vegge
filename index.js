77e7fec71a8d954e47a08d6d75dc85d6f1ae0f5e
El88Aleman <113607297+El88Aleman@users.noreply.github.com>
2025-01-28 16:52:48 -0300

    primer commit del backend

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mercadopago = require("mercadopago");
+require("dotenv").config();
+
+mercadopago.configure({
+  access_token: process.env.ACCESS_TOKEN,
+});
+
+app.use(express.json());
+app.use(cors({ origin: "https://www.clubvegge.com.ar" }));
+
+app.get("/", (req, res) => {
+  res.send("Todo Ok");
+});
+app.post("/create_preference", (req, res) => {
+  let preference = {
+    items: req.body.items,
+    back_urls: {
+      success: "https://www.clubvegge.com.ar/",
+      failure: "https://www.clubvegge.com.ar/",
+      pending: "",
+    },
+    auto_return: "approved",
+    shipments: {
+      cost: req.body.shipment_cost,
+      mode: "not_specified",
+    },
+  };
+
+  mercadopago.preferences
+    .create(preference)
+    .then(function (response) {
+      res.json({
+        id: response.body.id,
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+app.listen(8080, () => {
+  console.log("servidor corriendo");
+});
