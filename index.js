41a4b36fe890477e603ae4dc42cd224aa1477123
El88Aleman <113607297+El88Aleman@users.noreply.github.com>
2025-01-29 11:01:07 -0300

    cambios en actualizacion de mercadopago

--- index.js
+++ index.js
@@ -1,46 +1,49 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mercadopago = require("mercadopago");
-require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import { MercadoPagoConfig, Preference } from "mercadopago";
 
-mercadopago.configure({
+const client = new MercadoPagoConfig({
   access_token: process.env.ACCESS_TOKEN,
 });
 
-app.use(express.json());
+const app = express();
+const port = 8080;
+
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Todo Ok");
 });
-app.post("/create_preference", (req, res) => {
-  let preference = {
-    items: req.body.items,
-    back_urls: {
-      success: "https://www.clubvegge.com.ar/home",
-      failure: "https://www.clubvegge.com.ar/home",
-      pending: "",
-    },
-    auto_return: "approved",
-    shipments: {
-      cost: req.body.shipment_cost,
-      mode: "not_specified",
-    },
-  };
-
-  mercadopago.preferences
-    .create(preference)
-    .then(function (response) {
-      res.json({
-        id: response.body.id,
-      });
-    })
-    .catch(function (error) {
-      console.log(error);
+app.post("/create_preference", async (req, res) => {
+  try {
+    const body = {
+      items: [
+        {
+          title: req.body.title,
+          quantity: Number(req.body.quantity),
+          unit_price: Number(req.body.unit_price),
+          currency_id: "ARS",
+        },
+      ],
+      back_urls: {
+        success: "https://www.clubvegge.com.ar/home",
+        failure: "https://www.clubvegge.com.ar/home",
+        pending: "https://www.clubvegge.com.ar/home",
+      },
+      auto_return: "approved",
+    };
+    const preference = new Preference(client);
+    const result = await preference.create({ body });
+    res.json({
+      id: result.id,
     });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error al crear la preferencia :(" });
+  }
 });
 
-app.listen(8080, () => {
-  console.log("servidor corriendo");
+app.listen(port, () => {
+  console.log("servidor corriendo en el puerto ${port}");
 });
