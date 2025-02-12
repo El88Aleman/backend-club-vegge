6a2cf798f8867cd87e850cef3bc60a685215c9eb
El88Aleman <113607297+El88Aleman@users.noreply.github.com>
2025-02-12 15:41:48 -0300

    se fueron los errores de cors pero no funciona

--- index.js
+++ index.js
@@ -1,50 +1,51 @@
 import express from "express";
 import cors from "cors";
-import { MercadoPagoConfig, Preference } from "mercadopago";
+import dotenv from "dotenv";
+dotenv.config();
+import { MercadoPagoConfig } from "mercadopago";
 
 const client = new MercadoPagoConfig({
-  access_token:
-    "APP_USR-2047497174336582-012817-15a95deae18b9ac1a3cda92a99fa97fe-253417623",
+  accessToken: process.env.ACCESS_TOKEN,
 });
 
 const app = express();
-const port = 8080;
-
+const port = 3000;
 app.use(cors());
+app.options("*", cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Todo Ok");
+  res.send("Soy el server");
 });
+
 app.post("/create_preference", async (req, res) => {
+  console.log("Datos recibidos del frontend:", req.body);
   try {
     const body = {
-      items: [
-        {
-          title: req.body.title,
-          quantity: Number(req.body.quantity),
-          unit_price: Number(req.body.unit_price),
-          currency_id: "ARS",
-        },
-      ],
+      items: req.body.items.map((item) => ({
+        title: item.title,
+        quantity: Number(item.quantity),
+        unit_price: Number(item.unit_price),
+        currency_id: "ARS",
+      })),
       back_urls: {
-        success: "https://www.clubvegge.com.ar/home",
-        failure: "https://www.clubvegge.com.ar/home",
-        pending: "https://www.clubvegge.com.ar/home",
+        success: "https://www.clubvegge.com.ar/home/success/",
+        failure: "https://www.clubvegge.com.ar/home/failure/",
+        pending: "https://www.clubvegge.com.ar/home/pending/",
       },
       auto_return: "approved",
     };
+    console.log("Datos a enviar a MercadoPago:", body);
     const preference = new Preference(client);
     const result = await preference.create({ body });
-    res.json({
-      id: result.id,
-    });
+    console.log("Resultado de la creación de preferencia:", result);
+    res.json({ id: result.id });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Error al crear la preferencia :(" });
+    console.error("Error al crear la preferencia:", error);
+    res.status(500).json({ message: "Error al crear la preferencia" });
   }
 });
 
 app.listen(port, () => {
-  console.log("servidor corriendo en el puerto ${port}");
+  console.log(`El servidor del puerto esta corriendo ${port}`);
 });
